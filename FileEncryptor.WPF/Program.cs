a4dbc26bbae69c418e8ac7d6a0344a2dd85e303c
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-15 12:32:34 +0400

    Inversion of control

---
+++ FileEncryptor.WPF/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileEncryptor.WPF
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+
+        }
+    }
+}
