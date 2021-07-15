1ea757c26d93fb2d56ecb23c93fdf6d0aab5e5f1
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-15 12:01:03 +0400

    dev

---
+++ FileEncryptor.WPF/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Markup;
+using System.Windows.Threading;
+using System.Xaml;
+
+namespace FileEncryptor.WPF.ViewModels.Base
+{
+    internal class BaseViewModel : MarkupExtension, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+            var handlers = PropertyChanged;
+            if (handlers is null) return;
+
+            var invokation_list = handlers.GetInvocationList();
+            var arg = new PropertyChangedEventArgs(PropertyName);
+            foreach (var action in invokation_list)
+            {
+                if (action.Target is DispatcherObject disp_object)
+                    disp_object.Dispatcher.Invoke(action, this, arg);
+
+                else
+                    action.DynamicInvoke(this, arg);
+            }
+
+
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var value_target_service = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+            var root_object_service = serviceProvider.GetService(typeof(IProvideValueTarget)) as IRootObjectProvider;
+
+            OnInitialized(value_target_service?.TargetObject, value_target_service?.TargetProperty, root_object_service?.RootObject);
+
+            return this;
+        }
+
+        private WeakReference _TargetRef;
+        private WeakReference _RootRef;
+
+        public object TargetObject => _TargetRef.Target;
+        public object RootObject => _RootRef.Target;
+
+
+        protected virtual void OnInitialized(object Target, object Property, object Root)
+        {
+            _TargetRef = new WeakReference(Target);
+            _RootRef = new WeakReference(Root);
+        }
+
+    }
+}
---
+++ FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileEncryptor.WPF.ViewModels.Base;
+
+namespace FileEncryptor.WPF.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        #region Title : string - Title of Window
+
+        /// <summary>Title of Window</summary>
+        private string _Title = "Encryptor";
+
+        /// <summary>Title of Window</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
