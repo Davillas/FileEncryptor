e719ab3834c760bff41b2fa26b71157223ee18c0
David Akopyan <40228481+Davillas@users.noreply.github.com>
2021-07-15 12:27:19 +0400

    Commands

---
+++ FileEncryptor.WPF/Infrastructure/Commands/Base/BaseCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands.Base
+{
+    abstract class BaseCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private bool _Executable;
+
+        public bool Executable
+        {
+            get => _Executable;
+            set
+            {
+                if (_Executable == value) return;
+                _Executable = value;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter) => _Executable && CanExecute(parameter);
+
+        void ICommand.Execute(object parameter)
+        {
+            if(CanExecute(parameter))
+                Execute(parameter);
+        }
+
+        protected virtual bool CanExecute(object parameter) => true;
+
+        protected abstract void Execute(object parameter);
+
+    }
+}
---
+++ FileEncryptor.WPF/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileEncryptor.WPF.Infrastructure.Commands.Base;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands
+{
+    class LambdaCommand : BaseCommand
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action Execute, Func<bool> CanExecute = null) 
+            : this(p => Execute(), CanExecute is null ? (Func<object, bool>)null : p => CanExecute())
+        {
+            
+        }
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        protected override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        protected override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            _Execute(parameter);
+        }
+    }
+}
